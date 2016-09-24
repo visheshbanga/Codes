fe2940002b0b3d3bf14b2c3c366b1e9c4fd78c45
Vishesh Banga <visheshbanga@users.noreply.github.com>
2016-09-24 15:47:52 +0530

    Create rank.java

---
+++ BinaryTree/rank.java
@@ -0,0 +1,86 @@
+import java.util.*;
+class BST{
+    private Node root;
+
+    public BST(){
+        root = null;
+    }
+
+    private class Node{
+        int val,N;
+        Node right,left;
+        public Node(){
+            right = null;
+            left = null;
+            this.val = 0;
+            N = 1;
+        }
+        public Node(int val){
+            N = 1;
+            right = null;
+            left = null;
+            this.val = val;
+         
+        }
+    }
+
+    public void insert(int val){
+        root = insert(root,val);
+    }
+    
+    private Node insert(Node n,int val){
+        if(n == null)
+            return new Node(val);
+        if(n.val == val)
+            n.val = val;
+        if(n.val > val){
+            n.N++;
+            n.left = insert(n.left,val);
+        }
+        else{
+            n.N++;
+            n.right = insert(n.right,val);
+      }
+        return n;
+    }
+    
+    private int size(Node n){
+        if(n == null)
+            return 0;
+        return n.N;
+    }
+
+    public int findRank(int val){
+        return findRank(root,val);
+    }
+    
+    private int findRank(Node n,int val){
+        if(n == null)
+            return 0;
+        if(val < n.val){
+            return findRank(n.left,val);
+        }
+        else if(val > n.val){
+            return size(n.left) + 1 + findRank(n.right,val);
+        }
+        else
+            return size(n.left);
+    }
+        
+}
+public class rank{
+    public static void main(String args[]){
+        int num;
+        BST b = new BST();
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("Enter integer");
+            num = sc.nextInt();
+            if(num != -1)
+                b.insert(num);
+        }while(num != -1);
+        System.out.println("Enter value");
+        num = sc.nextInt();
+        System.out.println("Rank : " + b.findRank(num)); 
+    }
+}
