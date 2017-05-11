cc904cbcf5e144fcc04eed3017527e3f9716e6b0
Vishesh Banga <Vishesh Banga>
2017-05-11 21:06:02 +0530

    bug fix: in allpairShortestPath problem

--- Practice/allPairShortestPath/shortestPath.java
+++ Practice/allPairShortestPath/shortestPath.java
@@ -1,21 +1,28 @@
 import java.util.*;
 public class shortestPath{
-    public static void findPath(int graph[][], int v){
-        int path[][] = graph;
-        for(int k = 0; k < v; k++){
-            for(int i = 0; i < v; i++){
-                for(int j = 0; j < v; j++){
-                    if(path[i][k] != -1 && path[k][j] != -1)
-                        path[i][j] = Math.min(path[i][k]+path[k][j], path[i][j]);
-                }
-            }
-        }
-        for(int i = 0; i < v; i++){
+    static void print(int [][]path,int v){
+    	for(int i = 0; i < v; i++){
             for(int j = 0; j < v; j++){
                 System.out.print(path[i][j] + " ");
             }
             System.out.println();
         }
+    }
+    public static void findPath(int graph[][], int v){
+        int path[][] = graph;
+        for(int k = 0; k < v; k++){
+            for(int i = 0; i < v; i++){
+                for(int j = 0; j < v; j++){
+                    if(path[i][k] != -1 && path[k][j] != -1){
+                        if(path[i][j] != -1)
+                            path[i][j] = Math.min(path[i][k]+path[k][j], path[i][j]);
+                        else
+                            path[i][j] = path[i][k]+path[k][j];
+                    }
+                }
+            }
+        }
+        print(path,v);
     }
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
